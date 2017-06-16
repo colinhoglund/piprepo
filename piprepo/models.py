6ddc317d1282c3bb837f64f25dcc2bbec5318150
Colin Hoglund <colinhoglund@gmail.com>
2017-06-16 14:13:48 -0400

    using subcommands and not wrappering pip

--- piprepo/models.py
+++ piprepo/models.py
@@ -1,12 +1,9 @@
-import abc
 import boto3
 import errno
-import logging
+# import logging
 import os
 import re
-import shutil
-import subprocess
-import tempfile
+# import subprocess
 try:
     # python3
     from urllib.parse import urlparse
@@ -16,9 +13,8 @@ except ImportError:
 
 
 class Index(object):
-    ''' Abstract class that defines index interface '''
+    ''' Base context manager for building local indexes '''
 
-    __metaclass__ = abc.ABCMeta
     html_header = '<!DOCTYPE html><html><body>\n'
     html_footer = '</body></html>\n'
 
@@ -27,13 +23,12 @@ class Index(object):
         self.index_root = directory.rstrip('/') + '/simple/'
         self.packages = {}
 
-    @abc.abstractmethod
-    def __enter__():
-        pass
+    def __enter__(self):
+        self.build_local_packages()
+        return self
 
-    @abc.abstractmethod
-    def __exit__():
-        pass
+    def __exit__(self, exception_type, exception_value, traceback):
+        self.write_html()
 
     def write_html(self):
         # create index directories
@@ -62,9 +57,12 @@ class Index(object):
         with open(filename, 'w') as f:
             f.writelines(lines)
 
-    def build_packages(self):
-        files = [f for f in os.listdir(self.directory) if os.path.isfile(self.directory + f)]
-        for p in files:
+    def build_local_packages(self):
+        self._build_packages([f for f in os.listdir(self.directory)
+                              if os.path.isfile(self.directory + f)])
+
+    def _build_packages(self, packages):
+        for p in packages:
             name = os.path.basename(p).split('-')[0]
             # normalize name as per https://www.python.org/dev/peps/pep-0503/
             name = re.sub(r"[-_.]+", "-", name).lower()
@@ -74,79 +72,30 @@ class Index(object):
                 self.packages[name] = [p]
 
 
-class RemoteIndex(Index):
-    ''' Abstract class that defines remote index interface '''
+class S3Index(Index):
+    ''' Context manager for creating an index in AWS S3 '''
+    conn = boto3.resource('s3')
 
-    __metaclass__ = abc.ABCMeta
-
-    def __init__(self, path):
-        super(RemoteIndex, self).__init__(tempfile.mkdtemp())
+    def __init__(self, source, url):
+        super(S3Index, self).__init__(source)
+        parsed_url = urlparse(url)
+        self.bucket = self.conn.Bucket(parsed_url.netloc)
+        self.prefix = parsed_url.path.strip('/') + '/'
 
     def __enter__(self):
+        self.build_local_packages()
         self.sync_from_remote()
         return self
 
     def __exit__(self, exception_type, exception_value, traceback):
         self.write_html()
         self.sync_to_remote()
-        shutil.rmtree(self.directory)
-
-    @abc.abstractmethod
-    def sync_from_remote():
-        pass
-
-    @abc.abstractmethod
-    def sync_to_remote():
-        pass
-
-
-class LocalIndex(Index):
-    ''' Context manager for creating an index on the local filesystem '''
-
-    def __init__(self, directory):
-        super(LocalIndex, self).__init__(directory)
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, exception_type, exception_value, traceback):
-        self.write_html()
-
-
-# Not sure this class is necessary, should probably just use rsync outside of piprepo
-class RsyncIndex(RemoteIndex):
-    ''' Context manager for creating an index over rsync '''
-
-    def __init__(self, path):
-        self.path = path.rstrip('/') + '/'
-        super(RsyncIndex, self).__init__(path)
 
     def sync_from_remote(self):
-        try:
-            subprocess.check_call(['rsync', '-a', self.path, self.directory])
-            self.build_packages()
-        except subprocess.CalledProcessError:
-            logging.info('Remote index does not exist, creating...')
-
-    def sync_to_remote(self):
-        subprocess.check_call(['rsync', '-a', self.directory, self.path])
-
-
-class S3Index(RemoteIndex):
-    ''' Context manager for creating an index in AWS S3 '''
-    conn = boto3.resource('s3')
-
-    def __init__(self, url):
-        parsed_url = urlparse(url)
-        self.bucket = self.conn.Bucket(parsed_url.netloc)
-        self.prefix = parsed_url.path.strip('/') + '/'
-        super(S3Index, self).__init__(url)
-
-    def sync_from_remote(self):
-        for o in self.bucket.objects.filter(Prefix=self.prefix):
-            filename = self.directory + o.Object().key.split(self.prefix)[1]
-            self._create_directory(os.path.dirname(filename))
-            o.Object().download_file(filename)
+        s3_packages = [os.path.basename(o.key)
+                       for o in self.bucket.objects.filter(Prefix=self.prefix)
+                       if 'html' not in o.key]
+        self._build_packages(s3_packages)
 
     def sync_to_remote(self):
         files = [path.rstrip('/') + '/' + f for path, _, files in os.walk(self.directory) for f in files]
