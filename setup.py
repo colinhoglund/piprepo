6d382c320b3d15b00f383b2a8d96b6a2a82ed046
Colin Hoglund <colinhoglund@gmail.com>
2017-06-17 09:42:33 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -32,6 +32,7 @@ setup(
         'Topic :: System :: Systems Administration',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
     ],
 )
