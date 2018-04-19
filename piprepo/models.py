34a325df763b3a558a10e77c1e7d6a6a1da820b5
William Walter <wwalter@singleplatform.com>
2018-04-19 12:28:01 -0400

    stripping leading / from s3 keys

--- piprepo/models.py
+++ piprepo/models.py
@@ -146,6 +146,6 @@ class S3Index(Index):
 
     # hidden helper methods
     def _put_object(self, filename, content_type):
-        key = self.prefix + filename.split(self.source)[1]
+        key = self.prefix + filename.split(self.source)[1][1:] #strip / from path name
         body = open(filename, 'rb')
         self.bucket.put_object(Key=key, Body=body, ContentType=content_type)
